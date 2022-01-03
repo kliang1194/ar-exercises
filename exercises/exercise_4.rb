9949130a40efd29eadbeaf118ccf9fdfddde3e67
Kyle <kliang1194@gmail.com>
2022-01-03 16:44:08 -0500

    Solve Exercise 4: Loading a subset of stores

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,19 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+
+surrey = Store.create(name: 'Surrey', annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+whistler = Store.create(name: 'Whistler', annual_revenue: 1900000, womens_apparel: false, mens_apparel: true)
+yaletown = Store.create(name: 'Yaletown', annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+@mens_stores = Store.where(mens_apparel: true)
+
+@mens_stores.each do |store|
+    puts "Men's Store Name: #{store.name}. Store Annual Revenue: #{store.annual_revenue}"
+end
+
+@womens_low_revenue = Store.where('annual_revenue < ?', 1000000).where(womens_apparel: true)
+
+@womens_low_revenue.each do |store|
+  puts "Women's Store Name: #{store.name}. Store Annual Revenue: #{store.annual_revenue}"
+end
