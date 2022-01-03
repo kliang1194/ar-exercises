d113a25ca152afb134ebd08c6a6735402ba5a11f
Kyle <kliang1194@gmail.com>
2022-01-03 16:55:34 -0500

    Solve Exercise 5: Calculations

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,6 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+puts "Total Revenue from the entire company: #{Store.sum(:annual_revenue)}" 
+puts "Average Revenue from the entire company: #{Store.average(:annual_revenue)}" 
+puts "Number of stores generating $1M or more in annual sales: #{Store.where('annual_revenue >= ?', 1000000).count()}" 
