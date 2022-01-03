68a0aae29cdfa167e3e739d3252434c24efad437
Kyle <kliang1194@gmail.com>
2022-01-03 17:06:02 -0500

    Solve Exercise 6: One-to-many associations

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,11 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Kyle", last_name: "Liang", hourly_rate: 50)
+@store1.employees.create(first_name: "Buzz", last_name: "Lightyear", hourly_rate: 40)
+@store1.employees.create(first_name: "Harry", last_name: "Potter", hourly_rate: 75)
+@store2.employees.create(first_name: "Hermione", last_name: "Granger", hourly_rate: 60)
+@store2.employees.create(first_name: "Draco", last_name: "Malfoy", hourly_rate: 65)
+@store2.employees.create(first_name: "Albus", last_name: "Dumbledore", hourly_rate: 70)
+
